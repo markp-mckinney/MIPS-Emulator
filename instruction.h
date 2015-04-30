c5bff2dd72d1d7ad32b2d617e5e18ff5ef810391
Sniper <markp.mckinney@gmail.com>
2015-04-29 20:44:17 -0700

    Derp

--- instruction.h
+++ instruction.h
@@ -2,13 +2,13 @@
 #define INSTRUCTION_H
 
 typedef struct {
-	short opcode;
-	short rs;
-	short rt;
-	short rd;
-	short shamt;
-	short funct;
-	int imm;
+   short opcode;
+   short rs;
+   short rt;
+   short rd;
+   short shamt;
+   short funct;
+   int imm;
 } Instruction;
 
 void decode(Instruction *instr, int *pc);
